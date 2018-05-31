11064dfdd821805c0c60be3bd563c8c77e84d334
williammuliawan <39168441+williammuliawan@users.noreply.github.com>
2018-05-31 14:29:44 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,18 +6,28 @@ class Main {
 
   public static void main(String[] args) {
     Timer timer = new Timer();
-    Scanner reader = new Scanner(System.in);  // Reading from System.in
+    Scanner reader = new Scanner(System.in);  
+    // Sees input 
     System.out.println("Enter how much time you have in minutes: ");
-    int totalTime = reader.nextInt(); // Scans the next token of the input as an int.
+    int totalTime = reader.nextInt(); 
+    //sets input to variable
     while(totalTime>360 || totalTime<10){
-    //once finished
+    //once finished, 
     if(totalTime > 360 || totalTime<10){
-      System.out.println("That is unreasonable. Please try again.");
+      
+      if(totalTime>360){
+        System.out.println("That is too much time. Try going under 360 minutes.");
+      }
+      else if(totalTime<10){
+        System.out.println("That is not enough time. Try at least 10 minutes.");
+      }
+      System.out.println("Please try again.");
       totalTime = reader.nextInt();
     }
     }
-    System.out.println("You have " + totalTime + " minutes or " + totalTime/60 + " hour(s) and " + totalTime%60 + " minutes");
+    System.out.println("You have " + totalTime + " minutes or " + totalTime/60 + " hour(s) and " + totalTime%60 + " minute(s)");
     reader.close();
+    totalTime = totalTime*60;
    /* long startTime = System.currentTimeMillis();
     totalTime = totalTime*60;
     while(startTime/1000<=totalTime){
@@ -26,13 +36,16 @@ class Main {
     }*/
     //Test timer, not functional
 
-    /*timer.scheduleAtFixedRate(new TimerTask() {
+    timer.scheduleAtFixedRate(new TimerTask() {
     @Override
     public void run() {
-      System.out.println(totalTime);
+      System.out.println(totalTime/60 + " minutes" + totalTime + " seconds");
+      if(totalTime<=0){
+        Timertask.shutdown();
+      }
     }
-  }, 1000, 1000);*/
-//other timer not working in repl.it, try later in other platform
+  }, 1000, 1000);
+//timer not working in repl.it, try later in other platform
 
     //TimeUnit.SECONDS.Sleep(1);
     //Drifts after a while; not reliable
@@ -40,5 +53,7 @@ class Main {
 
 
   }
+
+
 
 }
